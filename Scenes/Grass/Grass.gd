8dead8d9c1cb855b61330a4ac2ea08e0a520acad
tomy204 <tomy5924@gmail.com>
2023-03-17 21:29:40 +0100

    sheeps targtet and move to grass patches in range when hungry
    + reimported the sprite sheet with no filter to remove some bottom line graphic glitch

--- Scenes/Grass/Grass.gd
+++ Scenes/Grass/Grass.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var being_eaten = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +12,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Grass_body_entered(body):
+	if body.is_in_group("sheeps") and being_eaten == false and body.targeted_grass == self:
+		being_eaten = true
+		body.eat_grass()
--- Scenes/Sheep/Sheep.gd
+++ Scenes/Sheep/Sheep.gd
@@ -5,11 +5,15 @@ export var min_distance_with_nearby = 50 # minimum distance the sheep will try t
 export var center_factor = 1 # used to control the priority of the move to center direction
 export var avoid_factor = 1 # used to control the priority of the move to avoid direction
 export var avg_dir_factor = 1 # used to control the priority of the common direction
-var direction = Vector2.ZERO
-var nearby_sheep = []
+export var grass_factor = 10 # used to control the priority of the direction to  targeted grass patch
+var hunger = 2 # number of grass patches the sheep need to eat -> 0 means it's full and does not need to eat anymore
+var direction = Vector2.ZERO # direction to move to
+var nearby_sheep = [] # list of sheeps which are in the detection area
+var nearby_grass = [] #list of grass patches which are in the detection area
+var targeted_grass
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	move_and_slide(direction * walk_speed)
 
 
@@ -20,7 +24,7 @@ func calc_direction_to_center_of_mass_nearby():
 		center += sheep.position
 	center /= nearby_sheep.size()
 	
-	return position.direction_to(center)
+	return position.direction_to(center) * center_factor
 
 
 # will return a vector for the sheep to keep some distance with each other to avoid collidiong - Boids rules #2
@@ -30,7 +34,7 @@ func calc_direction_to_avoid_colliding_nearby():
 		if position.distance_to(sheep.position) <= min_distance_with_nearby:
 			avoid -= position.direction_to(sheep.position)
 	
-	return position.direction_to(avoid)
+	return position.direction_to(avoid) * avoid_factor
 
 
 # will return a vector for the sheep to heads towards the same direction as the nearby sheeps - Boids rules #3
@@ -40,8 +44,39 @@ func calc_velocity_nearby():
 		avg_direction += sheep.direction
 	avg_direction /= nearby_sheep.size()
 	
-	return avg_direction
+	return avg_direction * avg_dir_factor
 
+
+func calc_direction_to_nearest_grass():
+	if target_grass():
+		return position.direction_to(targeted_grass.position) * grass_factor
+	else:
+		return Vector2.ZERO
+
+
+# will target the nearest grass patch in range if none is targeted and sheep is hungry
+# return true if there is already a targeted patch or if the function targeted a new one otherwise flase
+func target_grass():
+	if hunger > 0 and targeted_grass == null and nearby_grass.size() > 0:
+		var closest
+		var distance = 99999
+		for grass in nearby_grass:
+			var calc_dist = position.distance_to(grass.position)
+			if calc_dist < distance:
+				closest = grass
+				distance = calc_dist
+		targeted_grass = closest
+		
+	return targeted_grass != null
+		
+
+
+func eat_grass():
+	# play animation
+	# wait a certain amount of time
+	targeted_grass.queue_free()
+	targeted_grass = null
+	hunger -= 1
 
 
 func _on_DetectionArea_body_entered(body):
@@ -55,8 +90,21 @@ func _on_DetectionArea_body_exited(body):
 
 
 func _on_UpdateMovementTimer_timeout():
- direction = (
-	calc_direction_to_center_of_mass_nearby() * center_factor
-	+ calc_direction_to_avoid_colliding_nearby() * avoid_factor
-	+ calc_velocity_nearby() * avg_dir_factor
-).normalized()
+	direction = (
+		calc_direction_to_center_of_mass_nearby() +
+		calc_direction_to_avoid_colliding_nearby() +
+		calc_velocity_nearby() +
+		calc_direction_to_nearest_grass()
+	).normalized()
+
+
+func _on_DetectionArea_area_entered(area):
+	if area.is_in_group("grass"):
+		nearby_grass.append(area)
+
+
+func _on_DetectionArea_area_exited(area):
+	if area.is_in_group("grass"):
+		nearby_grass.erase(area)
+		if area == targeted_grass:
+			targeted_grass = null
