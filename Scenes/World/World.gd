4e355be1f7e30d5cba68435f9274da8a8a00fb88
Maaack <marek.belski@gmail.com>
2023-03-19 12:08:18 -0700

    Spawns sheep slightly to the left of player. Removes some grasses that messed up sheep.

--- Scenes/World/World.gd
+++ Scenes/World/World.gd
@@ -6,6 +6,7 @@ signal sheep_exploded(sheep_name)
 signal sheep_assembled(sheep_name)
 
 export(float, 0, 1000) var spawn_range : float = 200
+export(Vector2) var spawn_offset : Vector2 = Vector2.ZERO
 
 onready var shepherd_node = $Shepherd
 var sheep_scene = preload("res://Scenes/Sheep/Sheep.tscn")
@@ -23,7 +24,7 @@ func reset_world():
 func add_sheep(sheep_name : String) -> void:
 	var sheep_instance = sheep_scene.instance()
 	var spawn_range_vector = Vector2(rand_range(-spawn_range,spawn_range),rand_range(-spawn_range,spawn_range))
-	sheep_instance.position = shepherd_node.position + spawn_range_vector
+	sheep_instance.position = shepherd_node.position + spawn_range_vector + spawn_offset
 	sheep_instance.sheep_name = sheep_name
 	sheep_instance.connect("normal_grass_eaten", self, "_on_sheep_ate_normal_grass", [sheep_name])
 	sheep_instance.connect("volatile_grass_eaten", self, "_on_sheep_ate_volatile_grass", [sheep_name])
