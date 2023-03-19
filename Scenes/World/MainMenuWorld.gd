05e880e1ae8e1e89d44c0193d8872ff39199ec65
Maaack <marek.belski@gmail.com>
2023-03-19 12:29:56 -0700

    Main menu background world :-)

---
+++ Scenes/World/MainMenuWorld.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+export(float, 0, 1000) var spawn_range : float = 200
+export(Vector2) var spawn_offset : Vector2 = Vector2.ZERO
+
+onready var camera_node = $Camera2D
+
+var sheep_scene = preload("res://Scenes/Sheep/Sheep.tscn")
+
+func _add_sheep():
+	var sheep_instance = sheep_scene.instance()
+	var spawn_range_vector = Vector2(rand_range(-spawn_range,spawn_range),rand_range(-spawn_range,spawn_range))
+	sheep_instance.position = camera_node.position + spawn_range_vector + spawn_offset
+	add_child(sheep_instance)
+	
+
+func _on_SheepTimer_timeout():
+	_add_sheep()
