5bc6933e4db5aeec077d112c9a918d826653b884
Maaack <marek.belski@gmail.com>
2023-03-19 12:53:34 -0700

    Opening scene with GWJ logo.

---
+++ Scenes/BaseLevelUI/Levels/OpeningScreen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+export(String, FILE, "*.tscn") var next_scene : String
+	SceneLoader.load_scene(next_scene)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel") or event.is_action_pressed("ui_select") or event.is_action_pressed("interact"):
+		next()
+		get_tree().set_input_as_handled()
