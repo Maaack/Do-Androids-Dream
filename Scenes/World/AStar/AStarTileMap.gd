23d61bf8d934cc243599bca9531c7acad2c6d858
Maaack <marek.belski@gmail.com>
2023-06-06 10:10:06 -0700

    Adds diagonal support and tiles in negative x & y.

--- Scenes/World/AStar/AStarTileMap.gd
+++ Scenes/World/AStar/AStarTileMap.gd
@@ -13,6 +13,15 @@ const DIAGONAL_DIRECTIONS := [
 	Vector2.LEFT + Vector2.UP,
 	Vector2.LEFT + Vector2.DOWN,
 	]
+enum ConnectPointModes {
+	CARDINAL_ONLY,
+	DIAGONAL_ONLY,
+	ALL,
+}
+
+export(ConnectPointModes) var connect_point_mode : int = ConnectPointModes.CARDINAL_ONLY
+# The amount of tiles one can go into the negatives
+export(Vector2) var point_offset = Vector2(pow(2,10), pow(2,10))
 
 var astar := AStar2D.new()
 
@@ -20,15 +29,21 @@ func _ready() -> void:
 	update()
 
 func is_in_bounds(point_position : Vector2) -> bool:
-	return point_position.x >= 0 and point_position.y >= 0
+	var point_position_offset = point_position + point_offset
+	return point_position_offset.x >= 0 and point_position_offset.y >= 0
 
 func get_point_index(point_position: Vector2) -> int:
 	if not is_in_bounds(point_position):
 		print('Error: AStarTileMap positions cannot be less than 0 on any axis')
+		return 0
+	var point_position_offset = point_position + point_offset
 	# Cantor pairing function
-	var a := int(point_position.x)
-	var b := int(point_position.y)
-	return (a + b) * (a + b + 1) / 2 + b
+	var a := int(point_position_offset.x)
+	var b := int(point_position_offset.y)
+	var result = (a + b) * (a + b + 1) / 2 + b
+	if result < 0:
+		print("(%d + %d) * (%d + %d + 1) / 2 + %d = %d" % [a, b, a, b, b, result])
+	return result
 
 func connect_neighboring_points(cell_vector : Vector2 , directions : Array, bidirectional : bool = true):
 	var center_point_index := get_point_index(cell_vector)
@@ -45,7 +60,11 @@ func connect_neighboring_points(cell_vector : Vector2 , directions : Array, bidi
 func connect_cardinals(cell_vector : Vector2) -> void:
 	connect_neighboring_points(cell_vector, CARDINAL_DIRECTIONS)
 
-func connect_diagonals(cell_vector) -> void:
+func connect_diagonals(cell_vector : Vector2) -> void:
+	connect_neighboring_points(cell_vector, DIAGONAL_DIRECTIONS)
+
+func connect_all(cell_vector : Vector2) -> void:
+	connect_neighboring_points(cell_vector, CARDINAL_DIRECTIONS)
 	connect_neighboring_points(cell_vector, DIAGONAL_DIRECTIONS)
 
 func create_pathfinding_points(points : Array, point_weight : float = 1.0) -> void:
@@ -55,7 +74,13 @@ func create_pathfinding_points(points : Array, point_weight : float = 1.0) -> vo
 			continue
 		astar.add_point(get_point_index(point_vector), point_vector, point_weight)
 	for point_vector in points:
-		connect_cardinals(point_vector)
+		match(connect_point_mode):
+			ConnectPointModes.CARDINAL_ONLY:
+				connect_cardinals(point_vector)
+			ConnectPointModes.DIAGONAL_ONLY:
+				connect_diagonals(point_vector)
+			ConnectPointModes.ALL:
+				connect_all(point_vector)
 
 func set_path_length(point_path: Array, max_distance: int) -> Array:
 	if max_distance < 0: 
