b040cb62e0d75d507f06d4ff028a16d833875922
Maaack <marek.belski@gmail.com>
2023-06-12 16:03:09 -0700

    Adds path spawner node.

---
+++ Scenes/PathHint/PathsSpawner.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+export(PackedScene) var path_hint_scene : PackedScene = preload("res://Scenes/PathHint/PathHintType1.tscn")
+
+func add_path(path_array : PoolVector2Array, start_position : Vector2 , end_position : Vector2):
+	var path_hint_instance = path_hint_scene.instance()
+	call_deferred("add_child", path_hint_instance)
+	path_hint_instance.start_position = start_position
+	path_hint_instance.end_position = end_position
+	path_hint_instance.path_points = path_array
+	var tween = get_tree().create_tween()
+	tween.tween_property(path_hint_instance, "modulate", Color.transparent, 1)
+	yield(tween, "finished")
+	path_hint_instance.queue_free()
